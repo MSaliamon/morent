98e1f2e849397d4483e082fbb3337a326752f013
MSaliamon <maksimsalamon181@gmail.com>
2025-01-28 14:05:41 +0200

    fix doble rout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,15 @@ import filter from '../../assets/home-page/svg/filter.svg';
 import like from '../../assets/home-page/svg/Like.svg';
 import Notification from '../../assets/home-page/svg/Notification.svg';
 import Settings from '../../assets/home-page/svg/Settings.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase';
 import { useState, useEffect } from 'react';
 
 function Header() {
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -37,6 +39,12 @@ function Header() {
     }
   };
 
+  const handleNavigation = (path) => {
+    if (location.pathname !== path) {
+      navigate(path);
+    }
+  };
+
   return (
     <>
       <header className="header">
@@ -47,9 +55,15 @@ function Header() {
           <form className="search">
             <label className="search__wrap">
               <img src={search} alt="search" className="search__wrap-img" />
-              <input type="text" placeholder='Search something here' className="search__wrap-input" />
+              <input
+                type="text"
+                placeholder="Search something here"
+                className="search__wrap-input"
+              />
             </label>
-            <a href="" className="search-filter"><img src={filter} alt="filter" className="search__img" /></a>
+            <a href="#" className="search-filter">
+              <img src={filter} alt="filter" className="search__img" />
+            </a>
           </form>
         </div>
         <div className="header-user">
@@ -59,17 +73,31 @@ function Header() {
           {user ? (
             <>
               <span>{username}!</span>
-              <button onClick={handleLogout}>Logout</button>
+              <button onClick={handleLogout} className="header-user__logout">
+                Logout
+              </button>
             </>
           ) : (
             <>
-              <Link to="SignUp">Sign Up</Link>
-              <Link to="SignIn">Sign In</Link>
+              <button
+                onClick={() => handleNavigation('/SignUp')}
+                disabled={location.pathname === '/SignUp'}
+                className="header-user__button"
+              >
+                Sign Up
+              </button>
+              <button
+                onClick={() => handleNavigation('/SignIn')}
+                disabled={location.pathname === '/SignIn'}
+                className="header-user__button"
+              >
+                Sign In
+              </button>
             </>
           )}
         </div>
       </header>
     </>
-  )
+  );
 }
-export default Header
+export default Header;
